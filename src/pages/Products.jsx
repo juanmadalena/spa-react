346b303af832fb3537df03fb7cf1482a02b30fd1
Juan Madalena <juanmadalena06@gmail.com>
2023-11-20 10:45:32 +0100

    fix! links

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -12,8 +12,8 @@ export const Products = () => {
 
             <section className="w-full">
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium animi porro excepturi quam est iure enim, veniam repudiandae doloribus nemo eius necessitatibus ex architecto eligendi dignissimos nobis et minima tenetur?</p>
-                <p>This is a link to the <a href="./">Home</a> page.</p>
-                <p>This is a link to the <a href="./contact">Contact</a> page.</p>
+                <p>This is a link to the <Link to="/spa-react/">Home</Link> page.</p>
+                <p>This is a link to the <Link to="/spa-react/contact">Contact</Link> page.</p>
             </section>
         </>
     );
